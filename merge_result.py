66487b48a133729865117b6bf8b26f02664e3e86
Men Rui <809505910@qq.com>
2018-07-05 16:17:53 +0800

    更新合并脚本及gitignore

--- merge_result.py
+++ merge_result.py
@@ -1,15 +1,17 @@
 from collections import defaultdict
 score = defaultdict(float)
-output_f = open('rnn_result.csv','w')
+output_f = open('merge_rnn_rbf_svm_result.csv','w')
 sample_f = open('./data/submit_sample.csv','r')
-for i in range(10):
-
-    with open('output_5_'+str(i)+'.txt','r') as f:
+result_list = ['6.26_rnn_result.csv','test_result_rbf_svm.csv']
+weights = [1,0.1]
+for i, result_name in enumerate(result_list):
+    with open(result_name,'r') as f:
+        f.readline()
         for line in f:
             line = line.strip().split('\t')
             user_id = line[0]
             user_score = float(line[1])
-            score[user_id] += user_score
+            score[user_id] += user_score*weights[i]
 
 title = sample_f.readline()
 output_f.write(title)
