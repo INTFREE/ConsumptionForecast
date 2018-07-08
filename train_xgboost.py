d838d06ec484c9026e4bd056ee14022a7dec022e
Men Rui <809505910@qq.com>
2018-07-08 14:30:26 +0800

    更新训练脚本

--- train_xgboost.py
+++ train_xgboost.py
@@ -24,64 +24,53 @@ def split_data(user_ids, data):
 
 if __name__ == '__main__':
 
-    agg_res = read_agg_data(
+    train_agg = read_agg_data(
         train_dir + 'train_agg.csv',
         train_dir + 'train_log.csv'
     )
-    flg_res = read_flg_data(train_dir + 'train_flg.csv')
+    train_flg = read_flg_data(train_dir + 'train_flg.csv')
     test_agg = read_agg_data(
         test_dir + 'test_agg.csv',
         test_dir + 'test_log.csv'
     )
     print('load data end')
-    user_ids = list(agg_res.keys())
+    user_ids = list(train_agg.keys())
     test_user_ids = list(test_agg.keys())
 
-    for fold in range(folds):
-        np.random.shuffle(user_ids)
 
-        train_agg, valid_agg = split_data(user_ids, agg_res)
-        train_flg, valid_flg = split_data(user_ids, flg_res)
 
-        train_X = np.array([train_agg[key] for key in train_agg])
-        train_Y = np.array([train_flg[key] for key in train_agg])
+    #    train_agg, valid_agg = split_data(user_ids, agg_res)
+    #    train_flg, valid_flg = split_data(user_ids, flg_res)
 
-        valid_X = np.array([valid_agg[key] for key in valid_agg])
-        valid_Y = np.array([valid_flg[key] for key in valid_agg])
+    train_X = np.array([train_agg[key] for key in train_agg])
+    train_Y = np.array([train_flg[key] for key in train_agg])
 
-        test_X = np.array([test_agg[key] for key in test_agg])
+    test_X = np.array([test_agg[key] for key in test_agg])
 
-        print('train start')
-        classifier = XGBClassifier(learning_rate=0.1,
-                                   n_estimators=100,
-                                   max_depth=3,
-                                   min_child_weight=1,
-                                   gamma=0.5,
-                                   subsample=0.8,
-                                   colsample_bytree=0.8,
-                                   objective='binary:logistic',
-                                   nthread=4,
-                                   scale_pos_weight=1,
-                                   seed=27)
-        tuned_parameters = {'max_depth':[3,4,5,6,7,8]}
-        clf = GridSearchCV(classifier, param_grid=tuned_parameters, scoring='roc_auc', n_jobs=4, iid=False, cv=5)
-        clf.fit(train_X, train_Y)
-        print('best paras')
-        print(clf.best_params_)
-        #classifier.fit(train_X, train_Y)
-        print('train end')
+    print('train start')
+    cv_params = {'n_estimators': [100, 200, 300, 400, 500, 600, 700, 800]}
+    other_params = {'learning_rate': 0.1, 'max_depth': 5, 'min_child_weight': 1, 'seed': 0,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
-        train_prob = clf.predict_proba(train_X)[:, 1]
-        train_auc = cal_auc(train_Y, train_prob)
+    classifier = XGBClassifier(**other_params)
 
-        valid_prob = clf.predict_proba(valid_X)[:, 1]
-        valid_auc = cal_auc(valid_Y, valid_prob)
-        print('Train auc %f, Valid auc %f' % (train_auc, valid_auc))
+    clf = GridSearchCV(classifier, param_grid=cv_params, scoring='roc_auc', n_jobs=4, iid=False, cv=5)
+    clf.fit(train_X, train_Y)
+    print('best paras')
+    print('grid_scores:{0}'.format(clf.grid_scores_))
+    print('best paras{0}'.format(clf.best_params_))
+    print('best score:{0}'.format(clf.best_score_))
+    #classifier.fit(train_X, train_Y)
+    print('train end')
 
-        #xgboost.plot_tree(classifier, num_trees=0)
-        test_prob = clf.predict_proba(test_X)[:, 1]
-        with open('xgboost_result/%d.txt' % fold, 'w') as output:
-            for i, key in enumerate(test_agg):
-                output.write(str(key) + '\t' + str(test_prob[i]) + '\n')
-        break
-        #plt.savefig('800.png', dpi=800)
+    train_prob = clf.predict_proba(train_X)[:, 1]
+    train_auc = cal_auc(train_Y, train_prob)
+
+    print('Train auc %f' % (train_auc))
+
+    #xgboost.plot_tree(classifier, num_trees=0)
+    #test_prob = clf.predict_proba(test_X)[:, 1]
+    #with open('xgboost_result.txt', 'w') as output:
+    #    for i, key in enumerate(test_agg):
+    #        output.write(str(key) + '\t' + str(test_prob[i]) + '\n')
+    #plt.savefig('800.png', dpi=800)
